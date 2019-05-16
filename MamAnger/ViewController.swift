1f307a4f7cfbfc6d212f203d0d8c278b22ea0ff0
Rahmadani Pratiwi <rahmadanidp@gmail.com>
2019-05-16 20:59:41 +0700

    Initial Commit

---
+++ MamAnger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MamAnger
+//
+//  Created by Rahmadani Pratiwi on 16/05/19.
+//  Copyright © 2019 Rahmadani Pratiwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
